1d25985a11f155a4818c2c18e7985db218e0a5b8
Adan Vizcarry <adanclintonvizcarrychoque@gmail.com>
2025-12-29 17:01:45 -0500

    en esta no pude

--- scripts/index-dom.js
+++ scripts/index-dom.js
@@ -2,6 +2,7 @@ import hamburgerMenu from "./dom/menu-hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { shortcuts } from "./dom/teclado.js";
 import { moveBall } from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
 
 const d = document;
 
@@ -9,6 +10,7 @@ d.addEventListener("DOMContentLoaded", e => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+    countdown("countdown", "Oct 17, 2026 16:21:50", "Feliz Cumpleaños Eden🤑");
 });
 
 d.addEventListener("keydown", e => {
